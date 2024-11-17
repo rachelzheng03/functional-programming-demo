50289e94bfcb5ae39d9ad4385152215c757531ed
RachelZheng <rzhe2003@gmail.com>
2024-11-16 21:49:22 -0500

    finished writing functions in main

--- src/main.hs
+++ src/main.hs
@@ -1,11 +1,42 @@
 -- Write printAMessage here
+printAMessage :: Show a => a -> IO ()
+printAMessage x = putStrLn (show x)
 
 -- Write division here
+division :: Double -> Double -> Maybe Double
+division x y
+    | x == y    = Nothing
+    | otherwise = Just (x / y)
 
 -- Write factorial here
+factorial :: Int -> Int
+factorial 0 = 1
+factorial 1 = 1
+factorial n = n * factorial (n - 1)
 
 -- Write factList here
+factList :: Int -> [Int]
+factList n = [factorial i | i <- [1..n]]
 
 -- Write merge here
+merge :: [Int] -> [Int] -> [Int]
+merge [] ys = ys
+merge xs [] = xs
+merge (x:xs) (y:ys)
+    | x <= y    = x : merge xs (y:ys)
+    | otherwise = y : merge (x:xs) ys
 
-main = print "Test" -- Replace this with your testing code
+main :: IO ()
+main = do
+    -- testing printAMessage
+    printAMessage "Hello, Haskell!"
+    -- testing division
+    print(division 10 5)
+    -- testing factorial
+    print(factorial 4)
+    -- testing factList
+    print(factList 4)
+    -- testint merge
+    let list1 = [1, 3, 5]
+    let list2 = [2, 4, 6]
+    print (merge list1 list2)
--- src/test.hs
+++ src/test.hs
@@ -1,2 +1,7 @@
 -- Feel free to experiment with Haskell in this file!
 -- This will not affect your assignment's grade
+addNums :: Int -> Int
+addNums 0 = 0
+addNums n = n + addNums (n - 1)
+
+main = print (addNums 10)
